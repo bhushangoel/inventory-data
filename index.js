5b5859baa50d40f89a894effadc6fad6701069d0
Bhushan Goel <bgoel1991@gmail.com>
2021-07-18 19:58:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ var router = jsonServer.router('data/db.json');
 var middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    "/category/css": "/css",
+    "/category/js": "/js",
+    "/category/ts": "/ts",
+    "/category/html": "/html",
+    "/category/dev-tools": "/dev-tools",
+}))
+
 server.use(router);
 server.listen(process.env.PORT || 4000, function () {
     console.log('JSON Server is running')
